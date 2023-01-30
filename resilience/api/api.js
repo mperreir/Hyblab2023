1ab3b8d05b151cb9ff9875b80275dfc6d2d5e49c
TristanMANIER <tristan.manier@etu.univ-nantes.fr>
2023-01-30 12:01:46 +0100

    Add file deleted (oups), Add .html, .css and .js for each pages

--- resilience/api/api.js
+++ resilience/api/api.js
@@ -1,31 +1,17 @@
 'use strict';
 
-const app = require('express');
-const { json } = require('express');
+const app = require('express')();
 const path = require('path');
 
-const express = require('express');
-const routeur = express.Router();
+// Sample endpoint that sends the partner's name
+app.get('/topic', function (req, res) {
+    let topic;
 
-module.export = (db) => {
     // Get partner's topic from folder name
-    let topic = path.basename(path.join(__dirname, '/..'))
+    topic = path.basename(path.join(__dirname, '/..'))
+    // Send it as a JSON object
+    res.json({ 'topic': topic });
+});
 
-    // Sample endpoint that sends the partner's name
-    routeur.get('/topic', function (req, res) {
-        // Send it as a JSON object
-        res.json({ 'topic': topic });
-    });
-
-    routeur.get('/vote', async function (req, res) {
-        const votes = await db.Vote.getAll();
-        res.json(votes);
-    });
-
-    routeur.post('/vote', async function (req, res) {
-        await db.Vote.addVote(req.id);
-    });
-
-    // Export our API
-    return routeur;
-}
+// Export our API
+module.exports = app;
