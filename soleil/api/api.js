6ecb1374969306bd97d1b8fd3e30127954aaf93b
Sean Bogosavac <bogosavac.sean@icloud.com>
2023-02-01 16:34:38 +0100

    Tweaked the address API

--- soleil/api/api.js
+++ soleil/api/api.js
@@ -3,38 +3,96 @@
 const express = require('express');
 const { NominatimJS } = require('nominatim-js');
 const app = express();
-const addresser = require('addresser');
+const wget = require('node-wget');
 
 const data = require('./data/densite.json');
 
 app.use(express.json());
 
+// Retrieves a correct match for the address and sends it
 app.get('/searchAddress', async function (req, res) {
-    let searchText = req.body['searchText'];
-
-    let results = await NominatimJS.search({
-        q: searchText
-    });
-
-    res.json(results);
+    res.json(await getAddressFromText(req.body['searchText']));
 });
 
+// Retrieves all data regarding the input
 app.get('/addressInfo', async function (req, res) {
-    let city = req.body["city"];
+    // Uses our parser to turn the text input into a valid address
+    let location = await getAddressFromText(req.body['searchText']);
 
-    let density = data.find(e => e["Libellé commune"] === city)["Degré de Densité"];
+    // Retrieves the density of the town the person lives in
+    let density = data.find(e => e["Libellé commune"] === location.address.town)["Degré de Densité"];
 
-    let address = await NominatimJS.search({
-        street: req.body['street'],
-        city: req.body['city'],
-        county: req.body['county'],
-        state: req.body['state'],
-        country: req.body['country'],
-        postalcode: req.body['postalcode']
+    res.json({location, 'density': density});
+});
+
+
+// Retrieve a legit address from text input
+async function getAddressFromText(text) {
+    const apiCall = await NominatimJS.search({
+        q: text
     });
 
-    res.json({address, 'density': density});
-});
+    let address = {
+        'address': "",
+        'zip_code': 0,
+        'town': ""
+    };
+
+    const addressInfo = apiCall[0].display_name.split(', ');
+    
+    // Security to avoid problems later on
+    if (addressInfo.pop() !== "France") return "Service not avaliable outside of France";
+
+    // We need to check if the returned address starts with a number, or is just the name of the street
+    if (addressInfo[0].match(/^\d/)) {
+        console.log("Number detected");
+        address = {
+            'address': addressInfo[0] + ", " + addressInfo[1],
+            'zip_code': addressInfo.pop(),
+            'town': addressInfo[3]
+        };
+    } else {
+        console.log("No number here");
+        address = {
+            'address': addressInfo[0],
+            'zip_code': addressInfo.pop(),
+            'town': addressInfo[2]
+        };
+    }
+
+    const result = {
+        'latitude': apiCall[0].lat,
+        'longitude': apiCall[0].lon,
+        'address': address
+    };
+
+    return result;
+}
+
+// TODO finir cette fonction
+function getRadiationData(latitude, longitude, altitude, date_begin, date_end, time_ref, summatization) {
+    const username = "supy.game@gmail.com";
+    const url = `https://www.soda-is.com/service/wps?Service=WPS&Request=Execute&Identifier=get_cams_radiation&version=1.0.0&DataInputs=latitude=${latitude};longitude=${longitude};altitude=${altitude};date_begin=${date_begin};date_end=${date_end};time_ref=${time_ref};summarization=${summatization};username=${username}&RawDataOutput=irradiation`;
+
+    wget({
+        url:  url,
+        dest: './data/temp/',
+        timeout: 2000
+    },
+    function (error, response, body) {
+        if (error) {
+            console.log('--- error:');
+            console.log(error);            // error encountered
+        } else {
+            console.log('--- headers:');
+            console.log(response.headers); // response headers
+            console.log('--- body:');
+            console.log(body);             // content of package
+        }
+    }
+);
+}
+//getRadiationData(44.083, 5.059, -999, "2017-01-01", "2017-01-05", "UT", "PT15M");
 
 // Export our API
 module.exports = app;
