7df306aa9165d03a1ce49d536fa1f8cd16f0eae7
e191487n <corenin.pinguet@etu.univ-nantes.fr>
2023-02-01 10:01:13 +0100

    Init & fix url on console log

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ var server = app.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Hyblab routing app listening at http://%s:%s', host, port)
+  console.log('Hyblab routing app listening at http://localhost:%s/alerte', port)
 
 })
