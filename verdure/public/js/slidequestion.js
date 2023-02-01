e5659cfd0c2cc96d9b8d298cd52afa7f29ef038a
Guilhem <guilhem.lomelet@etu.univ-nantes.fr>
2023-02-01 14:39:57 +0100

    First push

--- verdure/public/js/slidequestion.js
+++ verdure/public/js/slidequestion.js
@@ -1,31 +1,18 @@
 "use strict";
 
 // async init function (because of the awaits on fetches)
-const initSlide1 = async function(){
+const initSlide3 = async function(){
   // Get logo element
-  const logo = document.querySelector('#logo-hyblab');
+  const page = document.querySelector('#third-slide')
+  let question = document.querySelector('#question')
+    
 
-  // (Re)set initial scale of logo
-  logo.setAttribute('style', 'transform :scale(1);');
-
-  // Animate hyblab logo and make shrink on click
-  anime({
-    targets: '#logo-hyblab',
-    scale: 1.2,
-    easing: 'easeInOutQuad',
-    direction: 'alternate',
-    loop: true
-  });
-
-
-  // Add click listener
-  logo.addEventListener('click', () => {
-    anime({
-        targets: '#logo-hyblab',
-        scale: 0
-      });
-    swiper.slideNext()
-  });
+  page.addEventListener('click', () => {
+        console.log("OKK")
+        question.style.opacity = 100;
+        page.style.backgroundPositionY = "-5vh";
+  }
+  )
 
   // Retrieve the partner's topic from our API
   let response = await fetch('api/topic');
