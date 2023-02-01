4aab958582a782369d57734eafc00824c610b8e4
Inkkonu <killian.blain@outlook.com>
2023-02-01 16:50:20 +0100

    API for questions and choices

--- verdure/api/api.js
+++ verdure/api/api.js
@@ -3,6 +3,7 @@
 const app = require( 'express' )();
 const path = require('path');
 
+
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
     let topic;
@@ -12,6 +13,31 @@ app.get('/topic', function ( req, res ) {
     // Send it as a JSON object
     res.json({'topic':topic});
 } );
+
+const prompts = {
+    1: {
+        "question":"This is the first question",
+        "choices": ["This is the first choice of the first question", "This is the second choice of the first question"]
+    },
+    2: {
+        "question":"This is the second question",
+        "choices":["This is the first choice of the second question", "This is the second choice of the second question"]
+    }
+}
+
+app.get('/question/:questionId', function (req, res){
+   const questionId = parseInt(req?.params?.questionId, 10);
+   const question = prompts[questionId]["question"];
+    if(req.query.choiceId != null){
+        const choiceId = parseInt(req.query.choiceId, 10)-1;
+        const choice = prompts[questionId]["choices"][choiceId];
+        res.json({'question':question, 'choice':choice});
+   }
+    else{
+        const choices = prompts[questionId]["choices"];
+        res.json({'question':question, 'choices':choices});
+    }
+});
 
 // Export our API
 module.exports = app;
