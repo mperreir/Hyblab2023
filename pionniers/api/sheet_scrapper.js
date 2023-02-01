f76b41a7670f4d8f2b578a6c655d2387bab335c3
E191583S (Marin CHEVOLLEAU) <marin.chevolleau@etu.univ-nantes.fr>
2023-02-01 20:58:22 +0100

    Rework du sheet_scrapper.

--- pionniers/api/sheet_scrapper.js
+++ pionniers/api/sheet_scrapper.js
@@ -2,21 +2,18 @@ const fs = require('fs');
 
 const {google} = require('googleapis');
 
-async function exportJSON (req, res) {
-
+async function retrieveSheetJSON () {
+    // Create a new JWT client using the key file downloaded from the Google Developers Console
     const auth = new google.auth.GoogleAuth({
         keyFile: "./pionniers/api/credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     })
-
     // Create a client instance for auth
     const client = await auth.getClient();
-
     // Create an instance of Google Sheets API
     const googleSheets = google.sheets({version: 'v4', auth: client});
-
+    // Declare spreadsheet ID
     const spreadsheetId = "118QtiG4eghOpB2-SPFNLjMyZj-tJoBW4_5MPU7FOlpg";
-
     // Read rows from spreadsheet and return as JSON object (or undefined if error)
     try {
         const getRows = await googleSheets.spreadsheets.values.get({
@@ -31,17 +28,27 @@ async function exportJSON (req, res) {
     }
 }
 
+async function exportSheetJSON () {
+    // Retrieve data from Google Sheets
+    const data = await retrieveSheetJSON();
+    // Do nothing if no data
+    if (!data){
+        return;
+    }
+    // Write data to JSON file
+    const data_json = JSON.stringify(data, null, 2);
+    fs.writeFile('./pionniers/public/data/data.json', data_json, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+}
+
+async function readJSONFromServerFile () {
+    // Read data from JSON file and return as JSON object
+    const data = await fs.readFileSync('./pionniers/public/data/data.json');
+    return JSON.parse(data.toString());
+}
+
 setInterval(() => {
-    exportJSON().then(
-        (data) => {
-            if (!data){
-                return;
-            }
-            const data_json = JSON.stringify(data, null, 2);
-            fs.writeFile('./pionniers/public/data/data.json', data_json, (err) => {
-                if (err) throw err;
-                console.log('Data written to file');
-            });
-        }
-    );
+    exportSheetJSON().then(r => console.log("Data updated"));
 }, 1000 * 10);
