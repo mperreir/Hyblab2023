d54f9a48609a065d8221cbea942b9952261555a6
Sean Bogosavac <supy.game@gmail.com>
2023-02-01 11:20:46 +0100

    Added address get api

--- soleil/api/api.js
+++ soleil/api/api.js
@@ -1,17 +1,28 @@
 'use strict';
 
-const app = require( 'express' )();
-const path = require('path');
+const express = require('express');
+const app = express();
+const addresser = require('addresser');
+
+app.use(express.json())
 
 // Sample endpoint that sends the partner's name
-app.get('/topic', function ( req, res ) {
+app.get('/topic', function (req, res) {
     let topic;
 
     // Get partner's topic from folder name
     topic = path.basename(path.join(__dirname, '/..'))
     // Send it as a JSON object
-    res.json({'topic':topic});
-} );
+    res.json({ 'topic': topic });
+});
+
+app.get('/addressInfo', function (req, res) {
+    let address = req.body['address'];
+
+    density = 0;
+
+    res.json({address, 'density': density});
+});
 
 // Export our API
 module.exports = app;
