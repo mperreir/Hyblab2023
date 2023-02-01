4a5c22db9a32aac17544897a690ddd88ab06ce83
Sean Bogosavac <supy.game@gmail.com>
2023-02-01 12:19:11 +0100

    API now returns density for the entered address

--- soleil/api/api.js
+++ soleil/api/api.js
@@ -4,22 +4,16 @@ const express = require('express');
 const app = express();
 const addresser = require('addresser');
 
+const data = require('./data/densite.json');
+
 app.use(express.json())
-
-// Sample endpoint that sends the partner's name
-app.get('/topic', function (req, res) {
-    let topic;
-
-    // Get partner's topic from folder name
-    topic = path.basename(path.join(__dirname, '/..'))
-    // Send it as a JSON object
-    res.json({ 'topic': topic });
-});
 
 app.get('/addressInfo', function (req, res) {
     let address = req.body['address'];
 
-    density = 0;
+    let city = address["city"];
+
+    let density = data.find(e => e["Libellé commune"] === city)["Degré de Densité"];
 
     res.json({address, 'density': density});
 });
