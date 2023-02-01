d18699c573260aa27914b3456fcffe0041d4816e
julien-raynal <julien.raynal@outlook.fr>
2023-02-01 14:30:41 +0100

    search address API Nominatim (OSM)

--- soleil/api/api.js
+++ soleil/api/api.js
@@ -1,19 +1,37 @@
 'use strict';
 
 const express = require('express');
+const { NominatimJS } = require('nominatim-js');
 const app = express();
 const addresser = require('addresser');
 
 const data = require('./data/densite.json');
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/addressInfo', function (req, res) {
-    let address = req.body['address'];
+app.get('/searchAddress', async function (req, res) {
+    let searchText = req.body['searchText'];
 
-    let city = address["city"];
+    let results = await NominatimJS.search({
+        q: searchText
+    });
+
+    res.json(results);
+});
+
+app.get('/addressInfo', async function (req, res) {
+    let city = req.body["city"];
 
     let density = data.find(e => e["Libellé commune"] === city)["Degré de Densité"];
+
+    let address = await NominatimJS.search({
+        street: req.body['street'],
+        city: req.body['city'],
+        county: req.body['county'],
+        state: req.body['state'],
+        country: req.body['country'],
+        postalcode: req.body['postalcode']
+    });
 
     res.json({address, 'density': density});
 });
