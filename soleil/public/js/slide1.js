30957799665666a68d4d6971213e038f0c2acf9f
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-01 12:19:46 +0100

    first visualisation of the first slide

--- soleil/public/js/slide1.js
+++ soleil/public/js/slide1.js
@@ -2,11 +2,18 @@
 
 // async init function (because of the awaits on fetches)
 const initSlide1 = async function(){
+  $(document).ready(function () {
+    $("#first-slide button").click(function(){
+      swiper.slideNext()
+      initSlide2();
+    });
+  });
+  
   // Get logo element
-  const logo = document.querySelector('#logo-hyblab');
+  //const logo = document.querySelector('#logo-hyblab');
 
   // (Re)set initial scale of logo
-  logo.setAttribute('style', 'transform :scale(1);');
+  /*logo.setAttribute('style', 'transform :scale(1);');
 
   // Animate hyblab logo and make shrink on click
   anime({
@@ -15,16 +22,17 @@ const initSlide1 = async function(){
     easing: 'easeInOutQuad',
     direction: 'alternate',
     loop: true
-  });
+  });*/
 
   // Add click listener
+  /** 
   logo.addEventListener('click', () => {
     anime({
         targets: '#logo-hyblab',
         scale: 0
       });
     swiper.slideNext()
-  });
+  });*/
 
   // Retrieve the partner's topic from our API
   let response = await fetch('api/topic');
