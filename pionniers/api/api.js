b274009713d6f3f2d519db4aa2ba350ccb84668b
E191583S (Marin CHEVOLLEAU) <marin.chevolleau@etu.univ-nantes.fr>
2023-02-02 17:45:30 +0100

    Création des routes nécessaires pour les fiches, les miniatures et la carte (back-end à priori fini)

--- pionniers/api/api.js
+++ pionniers/api/api.js
@@ -1,32 +1,37 @@
 'use strict';
 
 const app = require( 'express' )();
-const path = require('path');
 const sheet_scrapper = require('./sheet_scrapper');
 
-app.get('/map/all', function ( req, res ) {
+
+app.get('/map/keywords', function ( req, res ) {
     sheet_scrapper.readJSONFromServerFile().then(data => {
         // Filter data to only keep convenient fields
         data.values = data.values.map(row => {
             return {
-                "Id": row[0],
-                "Long": row[2],
-                "Lat": row[3],
-                "Topic": row[11],
                 "Keywords": row[12]
             }
         });
         // Remove header row
         data.values.shift();
-        // Remove range and majorDimension fields
-        delete data.range;
-        delete data.majorDimension;
+        // Remove empty rows
+        data.values = data.values.filter(row => row.Keywords);
+        // Trim values
+        data.values = data.values.map(row => {
+            row.Keywords = row.Keywords.trim();
+            return row;
+        });
+        // Retrieve values from the "Keywords" column as a list and with no duplicates
+        data.values = [...new Set(data.values.map(row => row.Keywords.split(";")).flat())];
+        // Order values alphabetically
+        data.values.sort();
         // Send it as a JSON object
         res.json(data.values);
     });
 });
 
-app.get('/map/filter/topics/:feed/:circular_economy/:energy/:industry/:mobility/:digital/keyword/:keyword?', function ( req, res ) {
+
+app.get('/map/topics/:feed/:circular_economy/:energy/:industry/:mobility/:digital/keyword/:keyword?', function ( req, res ) {
     sheet_scrapper.readJSONFromServerFile().then(data => {
         // Filter data to only keep convenient fields
         data.values = data.values.map(row => {
@@ -53,25 +58,109 @@ app.get('/map/filter/topics/:feed/:circular_economy/:energy/:industry/:mobility/
                 return row.Keywords.includes(req.params.keyword);
             });
         }
-        // Remove header row
-        data.values.shift();
-        // Remove range and majorDimension fields
-        delete data.range;
-        delete data.majorDimension;
         // Send it as a JSON object
         res.json(data.values);
     });
 });
 
-// Sample endpoint that sends the partner's name
-app.get('/topic', function ( req, res ) {
-    let topic;
 
-    // Get partner's topic from folder name
-    topic = path.basename(path.join(__dirname, '/..'))
-    // Send it as a JSON object
-    res.json({'topic':topic});
-} );
+app.get('/miniature/:id', function ( req, res ) {
+    sheet_scrapper.readJSONFromServerFile().then(data => {
+        // Filter data to only keep convenient fields
+        data.values = data.values.map(row => {
+            return {
+                "Id": row[0],
+                "Name": row[4],
+                "Company": row[5],
+                "City": row[6],
+                "Age": row[7],
+                "Status": row[8],
+                "MiniBio": row[9],
+                "Topic": row[11],
+                "Keywords": row[12],
+                "URLImage": row[15]
+            }
+        });
+        // Filter data to only keep the row with the requested id
+        data.values = data.values.filter(row => {
+            return row.Id === req.params.id;
+        });
+        // If no row was found, return an empty object
+        if (data.values.length === 0) {
+            data.values = {};
+            res.json(data.values);
+        }
+        else {
+            res.json(data.values[0]);
+        }
+    });
+});
+
+app.get('/miniature/topics/:feed/:circular_economy/:energy/:industry/:mobility/:digital?', function ( req, res ) {
+    sheet_scrapper.readJSONFromServerFile().then(data => {
+        // Filter data to only keep convenient fields
+        data.values = data.values.map(row => {
+            return {
+                "Id": row[0],
+                "Name": row[4],
+                "Company": row[5],
+                "City": row[6],
+                "Age": row[7],
+                "Status": row[8],
+                "MiniBio": row[9],
+                "Topic": row[11],
+                "Keywords": row[12],
+                "URLImage": row[15]
+            }
+        });
+        // Filter data to only keep rows with the requested topics
+        data.values = data.values.filter(row => {
+            return (req.params.feed === "true" && row.Topic.includes("alimentation")) ||
+                (req.params.circular_economy === "true" && row.Topic.includes("économie circulaire")) ||
+                (req.params.energy === "true" && row.Topic.includes("énergie")) ||
+                (req.params.industry === "true" && row.Topic.includes("industrie")) ||
+                (req.params.mobility === "true" && row.Topic.includes("mobilité")) ||
+                (req.params.digital === "true" && row.Topic.includes("numérique"));
+        });
+        // Send it as a JSON object
+        res.json(data.values);
+    });
+});
+
+app.get('/profile/:id', function ( req, res ) {
+    sheet_scrapper.readJSONFromServerFile().then(data => {
+        // Filter data to only keep convenient fields
+        data.values = data.values.map(row => {
+            return {
+                "Id": row[0],
+                "ExactAddress": row[1],
+                "Name": row[4],
+                "Company": row[5],
+                "Age": row[7],
+                "Status": row[8],
+                "ContentBio": row[10],
+                "Topic": row[11],
+                "Keywords": row[12],
+                "Podcast": row[13],
+                "Article": row[14],
+                "URLImage": row[15],
+                "URLLinkedin": row[16]
+            }
+        });
+        // Filter data to only keep the row with the requested id
+        data.values = data.values.filter(row => {
+            return row.Id === req.params.id;
+        });
+        // If no row was found, return an empty object
+        if (data.values.length === 0) {
+            data.values = {};
+            res.json(data.values);
+        }
+        else {
+            res.json(data.values[0]);
+        }
+    });
+});
 
 // Export our API
 module.exports = app;
