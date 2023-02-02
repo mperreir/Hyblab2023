af10a241af20b5d2fd39ed5ccde738de9c69f466
Inkkonu <killian.blain@outlook.com>
2023-02-02 22:13:39 +0100

    Changed API and prompts structure

--- verdure/api/api.js
+++ verdure/api/api.js
@@ -1,42 +1,58 @@
 'use strict';
 
-const app = require( 'express' )();
+const app = require('express')();
 const path = require('path');
 
 
 // Sample endpoint that sends the partner's name
-app.get('/topic', function ( req, res ) {
+app.get('/topic', function (req, res) {
     let topic;
 
     // Get partner's topic from folder name
     topic = path.basename(path.join(__dirname, '/..'))
     // Send it as a JSON object
-    res.json({'topic':topic});
-} );
+    res.json({'topic': topic});
+});
 
 const prompts = {
     1: {
-        "question":"This is the first question",
-        "choices": ["This is the first choice of the first question", "This is the second choice of the first question"]
+        "question": "This is the first question",
+        "choices": [{
+            "prompt": "This is the first choice of the first question",
+            "positive": ["First positive effect", "Second positive effect"],
+            "negative": ["First negative effect", "Second negative effect"],
+            "explication": "This is the explication"
+        },
+            {
+                "prompt": "This is the second choice of the first question",
+                "positive": ["First positive effect", "Second positive effect"],
+                "negative": ["First negative effect", "Second negative effect"],
+                "explication": "This is the explication"
+            }],
+        "nextQuestion": 2
     },
     2: {
-        "question":"This is the second question",
-        "choices":["This is the first choice of the second question", "This is the second choice of the second question"]
+        "question": "This is the second question",
+        "choices": [{
+            "prompt": "This is the first choice of the second question",
+            "positive": ["First positive effect", "Second positive effect"],
+            "negative": ["First negative effect", "Second negative effect"],
+            "explication": "This is the explication"
+        },
+            {
+                "prompt": "This is the second choice of the second question",
+                "positive": ["First positive effect", "Second positive effect"],
+                "negative": ["First negative effect", "Second negative effect"],
+                "explication": "This is the explication"
+            }],
+        "nextQuestion": null
     }
 }
 
-app.get('/question/:questionId', function (req, res){
-   const questionId = parseInt(req?.params?.questionId, 10);
-   const question = prompts[questionId]["question"];
-    if(req.query.choiceId != null){
-        const choiceId = parseInt(req.query.choiceId, 10)-1;
-        const choice = prompts[questionId]["choices"][choiceId];
-        res.json({'question':question, 'choice':choice});
-   }
-    else{
-        const choices = prompts[questionId]["choices"];
-        res.json({'question':question, 'choices':choices});
-    }
+app.get('/question/:questionId', function (req, res) {
+    const questionId = parseInt(req?.params?.questionId, 10);
+    const data = prompts[questionId];
+    res.json({'data': data});
 });
 
 // Export our API
