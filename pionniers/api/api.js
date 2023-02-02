b6320f4dea80244ebab6c2ea46f527d70c150cd8
E191583S (Marin CHEVOLLEAU) <marin.chevolleau@etu.univ-nantes.fr>
2023-02-02 15:46:38 +0100

    Changement des routes de la carte pour ne conserver qu'une route unique (prenant en compte les thématiques et les mots clés)

--- pionniers/api/api.js
+++ pionniers/api/api.js
@@ -26,7 +26,7 @@ app.get('/map/all', function ( req, res ) {
     });
 });
 
-app.get('/map/filter/keyword/:keyword', function ( req, res ) {
+app.get('/map/filter/topics/:feed/:circular_economy/:energy/:industry/:mobility/:digital/keyword/:keyword?', function ( req, res ) {
     sheet_scrapper.readJSONFromServerFile().then(data => {
         // Filter data to only keep convenient fields
         data.values = data.values.map(row => {
@@ -38,33 +38,7 @@ app.get('/map/filter/keyword/:keyword', function ( req, res ) {
                 "Keywords": row[12]
             }
         });
-        // Filter data to only keep rows with the requested keyword
-        data.values = data.values.filter(row => {
-            return row.Keywords.includes(req.params.keyword);
-        });
-        // Remove header row
-        data.values.shift();
-        // Remove range and majorDimension fields
-        delete data.range;
-        delete data.majorDimension;
-        // Send it as a JSON object
-        res.json(data.values);
-    });
-});
-
-app.get('/map/filter/topics/:feed/:circular_economy/:energy/:industry/:mobility/:digital', function ( req, res ) {
-    sheet_scrapper.readJSONFromServerFile().then(data => {
-        // Filter data to only keep convenient fields
-        data.values = data.values.map(row => {
-            return {
-                "Id": row[0],
-                "Long": row[2],
-                "Lat": row[3],
-                "Topic": row[11],
-                "Keywords": row[12]
-            }
-        });
-        // Filter data to only keep rows with the requested keyword
+        // Filter data to only keep rows with the requested topics
         data.values = data.values.filter(row => {
             return (req.params.feed === "true" && row.Topic.includes("alimentation")) ||
                 (req.params.circular_economy === "true" && row.Topic.includes("économie circulaire")) ||
@@ -73,6 +47,12 @@ app.get('/map/filter/topics/:feed/:circular_economy/:energy/:industry/:mobility/
                 (req.params.mobility === "true" && row.Topic.includes("mobilité")) ||
                 (req.params.digital === "true" && row.Topic.includes("numérique"));
         });
+        // Filter data to only keep rows with the requested keyword
+        if (req.params.keyword) {
+            data.values = data.values.filter(row => {
+                return row.Keywords.includes(req.params.keyword);
+            });
+        }
         // Remove header row
         data.values.shift();
         // Remove range and majorDimension fields
