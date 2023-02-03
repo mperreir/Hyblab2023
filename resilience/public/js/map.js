b47f92f0b6f6b9407f21743dd8ece4367816ad8c
maximefonteneau <fonteneau.max@gmail.com>
2023-02-03 21:23:05 +0100

    Ajout débloacage mairie

--- resilience/public/js/map.js
+++ resilience/public/js/map.js
@@ -1,21 +1,21 @@
 
-    var regions = document.querySelectorAll('[id^="_"]')
- 
-    regions.forEach(element => {
-        element.addEventListener('click', () => {
-            element.setAttribute('filter',"url(#saturation1)");
+var regions = document.querySelectorAll('[id^="_"]')
+const mairie = document.getElementById("_16");
+let compt = 0;
+
+regions.forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.id != "_16" || compt > 6) {
+            element.setAttribute('filter', "url(#saturation1)");
             str = element.id;
             nb_region = str.substr(1);
             path = "./dialogue.html?".concat(nb_region);
-            window.location=path
-        });
-    }); 
-    regions.forEach(element => {
-        element.addEventListener('click', () => {
-            element.setAttribute('filter',"url(#saturation1)");
-            str = element.id;
-            nb_region = str.substr(1);
-            path = "./dialogue.html?".concat(nb_region);
-            window.location=path
-        });
+            window.location = path
+            if (element.dataset.visite == "false") {
+                compt += 1
+                element.dataset.visite = "true"
+                console.log(compt)
+            }
+        }
     });
+});
