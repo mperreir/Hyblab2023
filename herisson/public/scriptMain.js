c450bf5f790977a0a98f202185761e481fedbc3f
Evan ACQUAIRE <evan.acquaire@etu.univ-nantes.fr>
2023-02-03 16:46:56 +0100

    Changement de fichiers divers

--- herisson/public/scriptMain.js
+++ herisson/public/scriptMain.js
@@ -21,3 +21,12 @@
 //         searchElement.action = 'commune.html';
 //     }
 // });
+
+const hammer = new Hammer(document.documentElement)
+hammer.on("panup", (event) => {
+    event.preventDefault();
+    document.body.style.opacity = "0";
+    setTimeout(() => {
+        window.location.href = "main.html";
+    }, 500);
+})
