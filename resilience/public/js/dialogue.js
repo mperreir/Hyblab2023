c61088d06ebd39d960298d85460d71dd14f90643
Tristan_Sama <tristan.manier@etu.univ-nantes.fr>
2023-02-04 19:48:05 +0100

    Nicer code with recursivity but still problem with eventlistener

--- resilience/public/js/dialogue.js
+++ resilience/public/js/dialogue.js
@@ -1,82 +1,94 @@
 
 let params = new URLSearchParams(window.location.search);
 let conversationId = params.get('id') || 1;
-let paused = false; // Is the click listener paused for the main line
 
-async function getConversation() {
-
+async function initConversation() {
     response = await fetch(`data/conversation${conversationId}.json`);
     const conversation = await response.json();
     const names = conversation.names;
 
-    let htmlDialogue = document.querySelector('#dialogue');
+    let htmlDialogueHead = document.querySelector('#head');
 
     let htmlNameLeft = document.createElement('p');
     htmlNameLeft.classList.add('name');
     htmlNameLeft.classList.add('left');
     htmlNameLeft.textContent = conversation.names[0];
-    htmlDialogue.appendChild(htmlNameLeft);
+    htmlDialogueHead.appendChild(htmlNameLeft);
     let htmlNameRigth = document.createElement('p');
     htmlNameRigth.classList.add('name');
     htmlNameRigth.classList.add('right');
     htmlNameRigth.textContent = conversation.names[1];
-    htmlDialogue.appendChild(htmlNameRigth);
+    htmlDialogueHead.appendChild(htmlNameRigth);
 
+
+    let htmlDialogue = document.querySelector('#dialogue');
+    let htmlDialogueMessages = document.querySelector('#messages');
+    runConversation(conversation.messages, names, htmlDialogue, htmlDialogueMessages);
+};
+
+function runConversation(messages, names, htmlDialogue, htmlDialogueMessages) {
+    let message;
+    let paused = false;
 
     let onClick = htmlDialogue.addEventListener('click', function () {
+        console.log(paused);
         if (!paused) {
-            message = conversation.messages.shift() || null;
-    
+            console.log("click");
+            paused = true;
+
+            message = messages.shift() || null;
+
             // If we run out of messages, stop listening to clicks
             if (message == null) {
                 htmlDialogue.removeEventListener('click', onClick);
-                console.log('end')
+                console.log('end');
                 return;
-            }
-    
-            createMessage(message, names, htmlDialogue);
-        }
+            };
+
+            switch (message.type) {
+                case 'message':
+                    displayMessage(message, names, htmlDialogue, htmlDialogueMessages);
+                    break;
+                case 'question':
+                    displayQuestion(message, names, htmlDialogue, htmlDialogueMessages);
+                    break;
+                case 'answer':
+                    displayAnswer(message, names, htmlDialogue, htmlDialogueMessages);
+                    break;
+                default:
+                    console.log('Unknown message type');
+            };
+
+            paused = false;
+        };
     });
-
 };
+                    
 
-function createMessage(message, names, htmlDialogue) {
-    switch (message.type) {
-        case 'message':
-            displayMessage(message, names, htmlDialogue);
-            break;
-        case 'question':
-            displayQuestion(message, names, htmlDialogue);
-            break;
-        case 'answer':
-            displayAnswer(message, names, htmlDialogue);
-            break;
-        default:
-            console.log('Unknown message type');
-    };
-};
-
-function displayMessage(message, names, htmlDialogue) {
-    let htmlMessages = document.createElement('p');
-    htmlMessages.classList.add('message');
+function displayMessage(message, names, htmlDialogue, htmlDialogueMessages) {
+    let htmlMessage = document.createElement('p');
+    htmlMessage.classList.add('message');
 
     const leftName = names[0];
     if (leftName == message.name) {
-        htmlMessages.classList.add('left');
+        htmlMessage.classList.add('left');
     }
     else {
-        htmlMessages.classList.add('right');
-    }
+        htmlMessage.classList.add('right');
+    };
 
-    htmlMessages.textContent = message.text;
-    htmlDialogue.appendChild(htmlMessages);
+    htmlMessage.textContent = message.text;
+    htmlDialogueMessages.appendChild(htmlMessage);
+};
 
-}
+function displayQuestion(message, names, htmlDialogue, htmlDialogueMessages) {
+    let htmlQuestionList = []; // Keep track of all the answers we create
 
-function displayQuestion(message, names, htmlDialogue) {
-    let htmlQuestionList = [] // Keep track of all the answers we create
-    paused = true;
+    // While there are questions to ask
+    //while (message.question.length > 0) {
+    for (let i = 0; i < message.question.length; i++) {
 
+    console.log(message.question);
     // Create each question
     message.question.forEach(questionLine => {
         let question = questionLine[0]; // First message of the sub-conversation is the question
@@ -85,61 +97,54 @@ function displayQuestion(message, names, htmlDialogue) {
         htmlQuestion.classList.add('answer');
         htmlQuestion.textContent = question.text;
 
-        htmlDialogue.appendChild(htmlQuestion);
+        htmlDialogueMessages.appendChild(htmlQuestion);
 
         htmlQuestionList.push(htmlQuestion);    // Add it to the list
-        
-        let onclick = htmlQuestion.addEventListener('click', function () {
-            let onClickSubConv = htmlDialogue.addEventListener('click', function () {
-                if (!paused) {
-                    message = questionLine.shift() || null;
-            
-                    // If we run out of messages, stop listening to clicks
-                    if (message == null) {
-                        htmlDialogue.removeEventListener('click', onClickSubConv);
-                        console.log('end sub conv')
-                        return;
-                    }
-            
-                    createMessage(message, names, htmlDialogue);
-                }
-            });
 
+        // When the question is clicked, start the sub-conversation
+        let onclick = htmlQuestion.addEventListener('click', function () {
             // Remove all the answers and stop listening to clicks
             htmlQuestionList.forEach(otherQuestion => {
                 otherQuestion.removeEventListener('click', onclick);
                 otherQuestion.remove();
             });
-            paused = false;
+
+            runConversation(questionLine, names, htmlDialogue);
+
+            message.question.splice(message.question.indexOf(questionLine), 1);
         });
     });
-}
+    console.log(message.question);
+    };
+};
 
 
-function displayAnswer(message, names, htmlDialogue) {
-    let htmlanswerList = [] // Keep track of all the answers we create
-    paused = true;
+function displayAnswer(message, names, htmlDialogue, htmlDialogueMessages) {
+    let htmlanswerList = []; // Keep track of all the answers we create
     // Create each answer
-    message.answer.forEach(answer => {
+    message.answer.forEach(answerLine => {
+        let answer = answerLine[0]; // First message of the sub-conversation is the answer
+
         let htmlAnswer = document.createElement('p');
         htmlAnswer.classList.add('answer');
         htmlAnswer.textContent = answer.text;
 
-        htmlDialogue.appendChild(htmlAnswer);
+        htmlDialogueMessages.appendChild(htmlAnswer);
 
-        htmlanswerList.push(htmlAnswer);    // Add it to the list
-        
         let onclick = htmlAnswer.addEventListener('click', function () {
-            createMessage(answer, names, htmlDialogue);
-
             // Remove all the answers and stop listening to clicks
             htmlanswerList.forEach(otherAnswer => {
                 otherAnswer.removeEventListener('click', onclick);
                 otherAnswer.remove();
             });
-            paused = false;
-        });
-    });
-}
 
-getConversation();
+            runConversation(answerLine, names, htmlDialogue);
+        });
+
+        htmlanswerList.push(htmlAnswer);    // Add it to the list
+    });
+};
+
+initConversation();
+
+// Path : http://localhost:8080/resilience/dialogue.html?id=2
