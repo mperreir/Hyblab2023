bb3798b9019226500432e08a0437331414e3b7cd
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-05 18:56:28 +0100

    add first case of the compas

--- soleil/public/js/slide5.js
+++ soleil/public/js/slide5.js
@@ -3,3 +3,12 @@
 const initSlide5 = function(){
 
 }
+
+function changeBoussole(){
+    switch($("#orientation img.boussole").atr("alt")){
+        case 0:
+            $("#orientation img.boussole").atr("alt", 1);
+            //$("#orientation img.boussole").atr("src", "bousole2");
+            break;
+    }
+}
