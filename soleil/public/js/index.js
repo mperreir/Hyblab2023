6670c89c46afc7585433283377bf3754cb81489c
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-05 23:06:02 +0100

    mise en forme de la plupart des slides

--- soleil/public/js/index.js
+++ soleil/public/js/index.js
@@ -4,10 +4,6 @@
 const swiper = new Swiper("#mySwiper", {
   direction: "vertical",
   mousewheel: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 });
 
 swiper.on("slideChange", function () {
