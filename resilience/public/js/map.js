f76609f9943e8fa816de75143848c3df10e92f51
maximefonteneau <fonteneau.max@gmail.com>
2023-02-06 11:18:24 +0100

    Ajout heure

--- resilience/public/js/map.js
+++ resilience/public/js/map.js
@@ -1,6 +1,8 @@
 
 var regions = document.querySelectorAll('[id^="_"]')
 const mairie = document.getElementById("_16");
+const heure = document.getElementById("heure").value;
+const heures = ['8:00','9:30','11:00','12:30','14:00','15:30','17:00','18:30','20:00']
 let compt = 0;
 
 regions.forEach(element => {
@@ -14,6 +16,7 @@ regions.forEach(element => {
             if (element.dataset.visite == "false") {
                 compt += 1
                 element.dataset.visite = "true"
+                document.getElementById("heure").value = " ".concat(heures[compt]," ");
                 console.log(compt)
             }
         }
