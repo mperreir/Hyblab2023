7437757a46d2eca109b298d87f74e94945f447b8
William Blot <william.blot@etu.univ-nantes.fr>
2023-02-06 13:40:57 +0100

    Ajout des fonctionnalités sqlite pour les appels de donnée

--- communes/api/api.js
+++ communes/api/api.js
@@ -3,6 +3,16 @@
 const app = require( 'express' )();
 const path = require('path');
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./communes/api/communes.db', sqlite3.OPEN_READWRITE, function (err) {
+    if (err) {
+        console.error(err + '\n' + 'run "npm run createDB" to create a database file');
+        // Pas de problème pour faire un appel synchrone ici : on est dans la phase
+        // d'initialisation du serveur et pas dans le traitement de requêtes.
+        require('process').exit(-1);
+    }
+});
+
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
     let topic;
@@ -12,6 +22,23 @@ app.get('/topic', function ( req, res ) {
     // Send it as a JSON object
     res.json({'topic':topic});
 } );
+
+app.get('/energy/:name', function ( req, res ) {
+    let energy = req.params.name;
+
+    console.log(energy);
+
+    db.all('SELECT * FROM Utilisateur', function (err, rows) {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        }
+        else {
+            console.log(rows);
+            res.json(rows);
+        }
+    });
+});
 
 // Export our API
 module.exports = app;
