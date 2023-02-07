81816a8d448303159b258e6f43a44f9495900bac
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-07 21:39:59 +0100

    interaction de logement

--- soleil/public/js/slide3.js
+++ soleil/public/js/slide3.js
@@ -6,14 +6,24 @@ const initSlide3 = async function(){
     if(!executed3){
         executed3 = true;
         $("#logement footer button").click(function(){
-            console.log($("#logement input[type=radio][name=logement]:checked").val());
-            if (true) { // vérifier que l'input radio est tjrs coché ?
-                quiz["logement"] = $("#logement input[type=radio][name=logement]:checked").val();
-                quiz["propriétaire"] = $("#logement input[type=radio][name=propriétaire]:checked").val();
+            if ($(".choix-logement img[class=selection]").length) { // vérifier que l'input radio est tjrs coché ?
+                quiz["logement"] = $(".choix-logement img[class=selection]").eq(0).attr("title");
+                quiz["propriétaire"] = $(".choix-logement img[class=selection]").eq(1).attr("title");
                 swiper.slideNext();
+                console.log(quiz);
             }
             
             });
+        $(".choix-logement img").click(function(){
+            
+            if($(this).attr("class") == "non-selection"){
+                $(this).attr("src", `img/Choix habitation/selection/${$(this).attr("title")}-selection.jpg`);
+                $(this).attr("class","selection");
+                let other_img = $(this).siblings()[1].title;
+                 $(`.choix-logement img[title=${other_img}]`).attr("class", "non-selection");
+                $(`.choix-logement img[title=${other_img}]`).attr("src", `img/Choix habitation/Non selection/${other_img}-non-selection.jpg`);
+            }
+        })
     }
     
 }
--- soleil/public/js/slide5.js
+++ soleil/public/js/slide5.js
@@ -12,9 +12,9 @@ const initSlide5 = function(){
 }
 
 function changeBoussole(){
-    switch($("#orientation img.boussole").atr("alt")){
+    switch($("#orientation img.boussole").attr("alt")){
         case 0:
-            $("#orientation img.boussole").atr("alt", 1);
+            $("#orientation img.boussole").attr("alt", 1);
             //$("#orientation img.boussole").atr("src", "bousole2");
             break;
     }
