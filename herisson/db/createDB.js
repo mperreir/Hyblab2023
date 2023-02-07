3ea3923077d88daf90f9e5abf08a76c8634423fe
Lucas_Trp <trapon.lucas55@gmail.com>
2023-02-07 15:36:05 +0100

    finalisation de toutes les BD

--- herisson/db/createDB.js
+++ herisson/db/createDB.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 
 const departements = [40, 24, 33, 17, 79, 86, 16, 19, 23, 87, 47, 64];
 
+
 async function getData(codeINSEE) {
     const url = `https://observatoire-fauna.fr/api/sudouest_especes_menacees_autour_ma_commune?commune=${codeINSEE}`;
     const response = await axios.get(url);
@@ -30,7 +31,21 @@ async function getAllData() {
         }
     }
     await Promise.all(promises);
-    const jsonData = JSON.stringify(allData);
+    // only keep the data with the category ['Mammifères', 'Amphibiens', 'Reptiles', 'Odonates', 'Oiseaux', 'Rhopalocères', 'Chiroptères'];
+    // the Mammifères category is named 'Mammifères (non volant)' in the API so change the name
+    const categories = ['Mammifères (non volant)', 'Amphibiens', 'Reptiles', 'Odonates', 'Oiseaux', 'Rhopalocères', 'Chiroptères'];
+    for (const key in allData) {
+        const value = allData[key];
+        for (const category in value) {
+            if (!categories.includes(category)) {
+                delete value[category];
+            }
+            else if (category === 'Mammifères (non volant)') {
+                value['Mammifères'] = value[category];
+                delete value[category];
+            }
+        }
+    }
 
     fs.writeFileSync("../public/data/db.json", JSON.stringify(allData));
 }
