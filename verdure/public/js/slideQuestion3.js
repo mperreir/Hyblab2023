b57a448e5a399c29800ea2f2ed95c8f1ae97f89b
Rafael Tillé <rafaeltille@etu.univ-nates.fr>
2023-02-07 11:48:09 +0100

    mise en ordre des zones

--- verdure/public/js/slideQuestion3.js
+++ verdure/public/js/slideQuestion3.js
@@ -9,7 +9,25 @@ const initSlideQuestion3 = async function(currentQuestion){
   let question = document.querySelector('.question-footer'+currentQuestion)
   let city = document.querySelector('#city'+currentQuestion)
 
-
+  var logo2 = document.querySelector('#logoQ3');
+  /*******
+     * GREEN Ton parking
+     */
+  
+    // (Re)set initial scale of logo
+    logo2.setAttribute('style', 'transform : translateY(-50vh);');
+    logo2.setAttribute('style', 'transform : scale(1);');
+    
+    // Animate hyblab logo and make shrink on click
+    anime({
+      targets: '.greenTa',
+      translateY: 0,
+      scale: 1.2,
+      easing: 'easeOutBounce',
+      loop : true,
+      direction: "alternate"
+    });
+  
 
   setTimeout(()=> {
    question.style.opacity = 100;
