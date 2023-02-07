95082ea5f2aa360a1b10974f4936c8ea391ac35f
William Blot <william.blot@etu.univ-nantes.fr>
2023-02-07 17:21:26 +0100

    Ajout d'un paramètre pour selectionner ce que l'on veut colorer

--- communes/public/js/change_svg_color.js
+++ communes/public/js/change_svg_color.js
@@ -1,11 +1,11 @@
-const change_svg_color = function (objectElement, color) {
+const change_svg_color = function (objectElement, color, class_to_change = '.st0') {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', objectElement.data, true);
     xhr.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             const parser = new DOMParser();
             const svgDoc = parser.parseFromString(this.responseText, 'image/svg+xml');
-            const elements = svgDoc.querySelectorAll('.st0');
+            const elements = svgDoc.querySelectorAll(class_to_change);
             for (let i = 0; i < elements.length; i++) {
                 elements[i].style.fill = color;
             }
