44d4dad8b24244386827b2bdca729b5bf183e819
Lucas_Trp <trapon.lucas55@gmail.com>
2023-02-07 23:40:42 +0100

    changements sur la carte

--- herisson/public/js/map.js
+++ herisson/public/js/map.js
@@ -1,9 +1,9 @@
 const createMap = async function (taxon) {
-    const map = L.map('map').setView([45.7, 0.3], 7);
+    const map = L.map('map').setView([45, 0.3], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        minZoom: 7,
-        maxZoom: 10,
+        minZoom: 8,
+        maxZoom: 11,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
@@ -22,41 +22,14 @@ const createMap = async function (taxon) {
     console.log(animalData);
     let geojson;
 
-    $.getJSON("https://france-geojson.gregoiredavid.fr/repo/regions/nouvelle-aquitaine/communes-nouvelle-aquitaine.geojson", function (data) {
-        const geoData = data.features.map(feature => {
-            const match = animalData.listCities.find(city => feature.properties.nom === city.commune);
-            if (match) {
-                feature.properties.nb_obs = match.nb_obs;
-                return feature;
-            }
-        }).filter(Boolean);
-
-        geojson = L.geoJSON(geoData, {
-            style: feature => ({
-                fillColor: getColor(feature.properties.nb_obs),
-                weight: 1.5,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: 0.7,
-            }),
-            onEachFeature: (feature, layer) => {
-                layer.on({
-                    mouseover: highlightFeature,
-                    mouseout: resetHighlight
-                });
-            }
-        }).addTo(map);
-    });
-
     function getColor(d) {
         return d > 500 ? '#661852' :
-            d > 100 ? '#99237A' :
-                d > 50 ? '#CC2FA3' :
-                    d > 25 ? '#D659B5' :
-                        d > 10 ? '#E082C8' :
-                            d > 5 ? '#FBC5EB' :
-                                d > 1 ? '#F5D5ED' :
+            d > 200 ? '#99237A' :
+                d > 100 ? '#CC2FA3' :
+                    d > 50 ? '#D659B5' :
+                        d > 25 ? '#E082C8' :
+                            d > 10 ? '#FBC5EB' :
+                                d > 5 ? '#F5D5ED' :
                                     '#FFF8F1';
     }
 
@@ -125,10 +98,30 @@ const createMap = async function (taxon) {
     };
 
     info.update = function (props) {
-        this._div.innerHTML = '<h4>Commune :</h4>' + (props ? '<b>' + props.nom + '</b><br />' + (props.nb_obs ? props.nb_obs + ' observations' : 'Aucune observation') : 'Cliquez sur une commune');
+        this._div.innerHTML = (props ? '<h4>' + props.nom + '</h4>' + (props.nb_obs ? props.nb_obs + ' observations' : 'Aucune observation') : 'Cliquez sur une commune');
     };
 
     info.addTo(map);
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 5, 10, 25, 50, 100, 200, 500],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
 
 
     geojson = L.geoJson(animalData, {
