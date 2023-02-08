4589ea7c4a7d7c2caa5b89b262a6e2b45fd24bce
Eligriv <virgile.siegler@laposte.net>
2023-02-08 20:03:59 +0100

    Page info

--- soleil/public/js/slide8.js
+++ soleil/public/js/slide8.js
@@ -4,5 +4,11 @@ var executed8 = false;
 const initSlide8 = async function(){
     if(!executed8){
         executed8 = true;
+        $("#facultatif footer button").eq(0).click(function(){
+            swiper.slideNext();
+        })
+        $("#facultatif footer .passer").click(function(){
+            swiper.slideNext();
+        })
     }
 }
--- soleil/public/js/slideInfo1.js
+++ soleil/public/js/slideInfo1.js
@@ -4,5 +4,11 @@ var executed10 = false;
 const initSlideInfo1 = async function(){
     if(!executedInfo1){
         executedInfo1 = true;
+        $("#facultatif footer button").eq(0).click(function(){
+            swiper.slideNext();
+        })
+        $("#facultatif footer .passer").click(function(){
+            swiper.slideNext();
+        })
     }
 }
