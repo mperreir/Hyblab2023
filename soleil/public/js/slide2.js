702e6673dfc550e11f5af86eeb911e7b63f8acd8
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-08 22:08:50 +0100

    ajout des propositions d'adresse

--- soleil/public/js/slide2.js
+++ soleil/public/js/slide2.js
@@ -5,13 +5,35 @@ var executed2 = false;
 const initSlide2 = async function(){
   if(!executed2){
     $("#adresse footer button").click(function(){
-      if (isValidAdress($("#adresse input").val())) {
-        quiz["adress"] = $("#adresse input").val();
-          swiper.slideNext();      
-      }
-  
-      
+        if (Object.keys(quiz).includes("adresse")){
+          swiper.slideNext(); 
+        }
+        else{
+          changeText();
+        }         
     });
+    $("#adresse input").change(function(){
+      getPossibleAdresse();
+    })
+    $('#adresse input').on('input', function() { // au changement de caractere
+      if($("#adresse input").val().charAt($("#adresse input").val().length - 1) == " "){
+        let pa =getPossibleAdresse();
+        $("div.result").html("");
+        pa.forEach(elt => {
+          $("div.result").append(`<option>${elt}</option>`)
+        });
+        
+        $("div.result").show();
+        $("div.result option").click(function(){
+          console.log("option");
+          quiz["adresse"] = $(this).html();
+          $('#adresse input').val($(this).html());
+          $("div.result").hide();
+        })
+      }
+  });
+  
+
     $("#adresse button.text").click(function(){
       console.log("button");
       changeText();
@@ -22,11 +44,19 @@ const initSlide2 = async function(){
   
 };
 
-function isValidAdress(adress){
-  return true;
-}
-
 function changeText(){
   console.log("ici");
-  $("#adresse commentaire-soli p").html("Vous me trouvez trop curieux ? Votre adresse va servir à calculer votre potentiel d’ensoleillement.")
+  $("#adresse .commentaire-soli p").html("Vous me trouvez trop curieux ? Votre adresse va servir à calculer votre potentiel d’ensoleillement.")
+}
+
+function getPossibleAdresse(s){
+  return ["bon","ici"];
+}
+
+function showOptions(){
+  $('option').show();
+}
+
+function hideOptions(){
+  $('option').hide();
 }
