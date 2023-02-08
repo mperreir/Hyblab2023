30ef9ffca38640c435db54cc4f39c86dd2f96e3b
Valentin MIGNOT <valentin.mignot@etu.univ-nantes.fr>
2023-02-08 15:34:52 +0100

    add the js to the animalList page pop-up

--- herisson/public/js/animalCategory.js
+++ herisson/public/js/animalCategory.js
@@ -1,21 +1,21 @@
-const animaldata = "Oiseaux";
+const animalCategory = "Oiseaux";
 const commune = "Bordeaux";
 const db = "data/db.json";
 const dbAdd = "data/additionalDB.json";
-const currentURL = window.location.href;
-console.log(currentURL);
-async function animalCategory() {
+
+async function animalCategoryJS() {
     /*function to create a dictionary of the wanted animals*/
-    async function getData(dbPath) {
+    async function getAllAnimals(dbPath) {
         const response = await fetch(dbPath);
         const data1 = await response.json();
-        return data1[commune][animaldata];
+        return data1[commune][animalCategory];
     }
 
     /*call it*/
-    const dataTest = await getData(db);
-    console.log(dataTest);
+    const animalsData = await getAllAnimals(db);
+    console.log(animalsData);
 
+    /*  main page */
     const footercontainer = document.getElementById("footerText")
     const h1container = document.getElementById("category")
     const citycontainer = document.getElementById("commune")
@@ -24,9 +24,9 @@ async function animalCategory() {
     const main = document.querySelector("main.main");
     const footer = document.querySelector("footer.main")
 
-    h1container.textContent = animaldata.split(" ").shift();
+    h1container.textContent = animalCategory.split(" ").shift();
     citycontainer.textContent = commune;
-    footercontainer.textContent = "Comment aider ces " + animaldata.toLowerCase() + " ?";
+    footercontainer.textContent = "Comment aider ces " + animalCategory.toLowerCase() + " ?";
     footercontainer.style.color = "#FBC5EB";
 
     buttonContainer.style.width = "innerWidth";
@@ -34,16 +34,16 @@ async function animalCategory() {
     buttonContainer.style.flexDirection = "column";
 
     /*create each animal's buttons */
-    for (const key in dataTest) {
+    for (const key in animalsData) {
         const button = document.createElement("button");
         button.className = "animalButton";
-        if(dataTest[key]["nom_vern"] == null){
-            animalName = dataTest[key]["lb_nom"];
+        if(animalsData[key]["nom_vern"] == null){
+            name = animalsData[key]["lb_nom"];
         } else {
-            animalName = dataTest[key]["nom_vern"];
+            name = animalsData[key]["nom_vern"];
         }
-        button.id = animalName;
-        button.textContent = animalName;
+        button.id = name;
+        button.textContent = name;
         button.style.margin = "2vw 0";
         buttonContainer.appendChild(button);
     }
@@ -56,18 +56,55 @@ async function animalCategory() {
         main.style.minHeight = '71.7vh';
     }
 
-    /* put the pop-up visible */
-    async function selectAnimal(){
-        const mainDiv = document.getElementById("main");
-        const popDiv = document.getElementById("pop-up");
-        const buttons = document.getElementsByClassName("animalButton");
-        for (let i = 0; i < buttons.length; i++) {
-            buttons.item(i).addEventListener("click", function(){
-                mainDiv.style.display = "none";
-                popDiv.style.display = "block";
-            });
+
+    /* pop-up */
+
+    async function getInfoPage(animal) {
+        async function getAnimalData(dbPath) {
+            const response = await fetch(dbPath);
+            const data1 = await response.json();
+            return data1[animal];
         }
+
+        /*call it*/
+        const animalData = await getAnimalData(dbAdd);
+        console.log(animalData);
+
+        const animalName = document.getElementById("animal-name");
+        const desc = document.getElementById("desc");
+        const menace = document.getElementById("menace-level");
+        const vus = document.getElementById("nb");
+        const map = document.getElementById("map");
+        const tips = document.getElementById("tips-text");
+        const imgDiv = document.getElementById("picture-div")
+        const img = document.getElementById("animal-picture");
+        const copyright = document.getElementById("copyright");
+
+        if (animal.length > 22) {
+            animalName.style.fontSize = '4vh';
+        } else if (animal.length > 27) {
+            animalName.style.fontSize = '3.5vh';
+        }
+        animalName.textContent = animal.toUpperCase();
+        desc.textContent = "Les " + animalData["listCities"][0]["categorie"].toLowerCase();
+        if (animalData["listCities"][0]["enjeu_conservation"] == null){
+            menace.textContent = "INCONNU"
+        } else {
+            menace.textContent = animalData["listCities"][0]["enjeu_conservation"].toUpperCase();
+        }
+        map.textContent = "Carte";
+        tips.textContent = "Découvrez comment vous pouvez aider les " + animalData["listCities"][0]["categorie"].toLowerCase() + " ?";
+        vus.textContent = animalData["listCities"][0]["nb_obs"];
+        img.src = "img/animals/" + animalData["id"] + ".jpg";
+        imgDiv.style.minHeight = '41vh';
+        imgDiv.style.maxHeight = '41vh';
+        imgDiv.style.maxWidth = '100vw';
+        imgDiv.style.width = "auto";
+        imgDiv.style.zIndex = "200";
+        copyright.textContent = animalData["copyright"];
     }
+
+
     /* quit the pop-up */
     async function quitPopUp(){
         const mainDiv = document.getElementById("main");
@@ -78,9 +115,23 @@ async function animalCategory() {
             popDiv.style.display = "none";
         })
     }
+    /* put the pop-up visible */
+    async function selectAnimal(){
+        const mainDiv = document.getElementById("main");
+        const popDiv = document.getElementById("pop-up");
+        const buttons = document.getElementsByClassName("animalButton");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons.item(i).addEventListener("click", function(){
+                mainDiv.style.display = "none";
+                popDiv.style.display = "block";
+
+                getInfoPage(buttons.item(i).id);
+            });
+        }
+    }
 
     await selectAnimal();
     await quitPopUp();
 }
 
-animalCategory();
+animalCategoryJS();
