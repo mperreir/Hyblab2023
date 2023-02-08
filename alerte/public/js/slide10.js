0a2d6356ef628616c4831e52edb0980c8e3b7e33
Corentin <corentin.pinguet@etu.univ-nantes.fr>
2023-02-08 20:07:43 +0100

    reponses s8 / 14 + add s14 / s15_1 / s15_2 + fix text + texts de réponses dynamiques q3 / q6 + fix btn

--- alerte/public/js/slide10.js
+++ alerte/public/js/slide10.js
@@ -7,3 +7,11 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
     output.innerHTML = this.value;
 }
+
+var slider14 = document.getElementById("myRange14");
+var output14 = document.getElementById("demo14");
+output14.innerHTML = slider14.value;
+
+slider14.oninput = function () {
+    output14.innerHTML = this.value;
+}
