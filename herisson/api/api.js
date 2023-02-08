e2d51f1ce90d8c88a77b85b0344578d95402062e
Marten Schuitemaker <marten.schuitemaker@etu.univ-nantes.fr>
2023-02-08 18:19:59 +0100

    mise à jour api

--- herisson/api/api.js
+++ herisson/api/api.js
@@ -3,9 +3,10 @@
 const fs = require("fs");
 const app = require( 'express' )();
 const path = require('path');
-const {flatten} = require("express/lib/utils");
 
-let database;
+let dbAnimal;
+let dbCommune;
+let dbTips;
 
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
@@ -17,21 +18,31 @@ app.get('/topic', function ( req, res ) {
     res.json({'topic':topic});
 } );
 app.get('/animal/autocomplete/:name', function ( req, res ) {
-    if (!database) {
-        database = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
+    if (!dbAnimal) {
+        dbAnimal = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
     }
-    const filteredNames = Object.keys(database)
-        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()));
-    console.log(filteredNames);
+    let count = 0;
+    const filteredNames = Object.keys(dbAnimal).filter(key => {
+        if (count >= 10) {
+            return false;
+        }
+        if (key.toLowerCase().startsWith(req.params.name.toLowerCase())) {
+            count++;
+            return true;
+        }
+        return false;
+    });
     res.json({filteredNames});
 });
 
 app.get('/animal/:name', function ( req, res ) {
-    const database = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
-    const filteredData = Object.keys(database)
-        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()))
+    if (!dbAnimal) {
+        dbAnimal = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
+    }
+    const filteredData = Object.keys(dbAnimal)
+        .filter(key => key.toLowerCase() === req.params.name.toLowerCase())
         .reduce((filteredData, key) => {
-            filteredData[key] = database[key];
+            filteredData[key] = dbAnimal[key];
             return filteredData;
         }, {});
 
@@ -39,34 +50,50 @@ app.get('/animal/:name', function ( req, res ) {
 });
 
 app.get('/commune/autocomplete/:name', function ( req, res ) {
-    //Time the request
-    if (!database) {
-        database = JSON.parse(fs.readFileSync('herisson/public/data/db.json').toString());
+    if (!dbCommune) {
+        dbCommune = JSON.parse(fs.readFileSync('herisson/public/data/db.json').toString());
     }
     let count = 0;
-    const filteredNames = Object.keys(database)
-        .filter(key => {
-            if (count < 10 && key.toLowerCase().startsWith(req.params.name.toLowerCase())) {
-                count++;
-                return true;
-            }else{
-                return false;
-            }
-        });
+    const filteredNames = Object.keys(dbCommune).filter(key => {
+        if (count >= 10) {
+            return false;
+        }
+        if (key.toLowerCase().startsWith(req.params.name.toLowerCase())) {
+            count++;
+            return true;
+        }
+        return false;
+    });
     res.json({filteredNames});
-
 });
 
 app.get('/commune/:name', function ( req, res ) {
-    const database = JSON.parse(fs.readFileSync('herisson/public/data/db.json').toString());
-    const filteredData = Object.keys(database)
-        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()))
+    if (!dbCommune) {
+        dbCommune = JSON.parse(fs.readFileSync('herisson/public/data/db.json').toString());
+    }
+    const filteredData = Object.keys(dbCommune)
+        .filter(key => key.toLowerCase() === req.params.name.toLowerCase())
         .reduce((filteredData, key) => {
-            filteredData[key] = database[key];
+            filteredData[key] = dbCommune[key];
             return filteredData;
         }, {});
 
     res.json({filteredData});
 });
+
+app.get('/tips/:name', function ( req, res ) {
+    if (!dbTips) {
+        dbTips = JSON.parse(fs.readFileSync('herisson/public/data/tipsDB.json').toString());
+    }
+    const filteredData = Object.keys(dbTips)
+        .filter(key => key.toLowerCase() === req.params.name.toLowerCase())
+        .reduce((filteredData, key) => {
+            filteredData[key] = dbTips[key];
+            return filteredData;
+        }, {});
+
+    res.json({filteredData});
+});
+
 // Export our API
 module.exports = app;
