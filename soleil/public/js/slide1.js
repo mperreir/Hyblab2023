1fa00cd01cf480146775ebcd8aaf86a004f47911
Sean Bogosavac <bogosavac.sean@icloud.com>
2023-02-09 11:17:34 +0100

    End of merge

--- soleil/public/js/slide1.js
+++ soleil/public/js/slide1.js
@@ -2,16 +2,6 @@
 var executed1 = false;
 let quiz = {}
 // async init function (because of the awaits on fetches)
-<<<<<<< HEAD
-const initSlide1 = async function () {
-  if (!executed1) {
-    executed1 = true;
-    $("#accueil button").click(function () {
-      swiper.slideNext();
-    });
-    swiper.slideNext()
-  };
-=======
 const initSlide1 = async function(){
   $("video").get(0).play();
     if(!executed1){
@@ -33,5 +23,4 @@ const initSlide1 = async function(){
 
   
 
->>>>>>> front-lea
 };
--- soleil/public/js/slide2.js
+++ soleil/public/js/slide2.js
@@ -2,37 +2,6 @@
 
 var executed2 = false;
 // Just animate the logo
-<<<<<<< HEAD
-const initSlide2 = async function () {
-  $("#adresse footer button").click(async function () {
-
-    if (isValidAdress($("#adresse input").val())) {
-      quiz["addressInput"] = $("#adresse input").val();
-
-      console.log(quiz["addressInput"]);
-
-      quiz["address"] = await fetch(`api/searchAddresses/${quiz["addressInput"]}`)
-        .then(swiper.slideNext());
-      initSlide3();
-    }
-
-
-  });
-  $("#adresse button.text").click(function () {
-    console.log("button");
-    changeText();
-  })
-  $("#adresse header button").click(function () {
-    swiper.slidePrev();
-  })
-};
-
-function isValidAdress(adress) {
-  return true;
-}
-
-function changeText() {
-=======
 const initSlide2 = async function(){
   if(!executed2){
     $("#adresse footer button").click(function(){
@@ -94,7 +63,6 @@ const initSlide2 = async function(){
 };
 
 function changeText(){
->>>>>>> front-lea
   console.log("ici");
   $("#adresse .commentaire-soli p").html("Vous me trouvez trop curieux ? Votre adresse va servir à calculer votre potentiel d’ensoleillement.")
 }
