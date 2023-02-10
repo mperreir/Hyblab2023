37c93fb7d0ea52b79664df650831c93340a67129
cleme <clement.daudin@etu.univ-nantes.fr>
2023-02-10 13:48:21 +0100

    monScore

--- communes/public/js/score.js
+++ communes/public/js/score.js
@@ -1,1 +1,8 @@
 document.querySelector(".headerThree p").innerHTML = "score : "+sessionStorage.getItem("Score");
+
+function updateScore(nbSlide){
+    monScore = sessionStorage.getItem("Score");
+    monScore = monScore.substring(0,monScore.length-1);
+    monNouveauScore = parseInt(monScore)+(100-parseInt(monScore)/nbSlide);
+    sessionStorage.setItem("Score", monNouveauScore.toString()+"%");
+}
