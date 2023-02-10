115a32f863899c484e133a31d1c2bd0a4307593f
cleme <clement.daudin@etu.univ-nantes.fr>
2023-02-10 13:52:05 +0100

    monScore correction

--- communes/public/js/caroussel.js
+++ communes/public/js/caroussel.js
@@ -71,10 +71,7 @@ fetch('../data/data.json')
     });
 
 function openEnv(){
-    let monscore = sessionStorage.getItem("Score");
-    monscore=monscore.substring(0,monscore.length-1);
-    let result = parseInt(monscore)+3;
-    sessionStorage.setItem("Score",result.toString()+"%");
+    updateScore(document.querySelectorAll(".carousel-item").length)
     let idEnv=document.querySelector(".active").id
     sessionStorage.setItem("visit",idEnv)
     location.href = "home.html"
--- communes/public/js/score.js
+++ communes/public/js/score.js
@@ -1,8 +1,12 @@
 document.querySelector(".headerThree p").innerHTML = "score : "+sessionStorage.getItem("Score");
 
 function updateScore(nbSlide){
-    monScore = sessionStorage.getItem("Score");
-    monScore = monScore.substring(0,monScore.length-1);
-    monNouveauScore = parseInt(monScore)+(100-parseInt(monScore)/nbSlide);
-    sessionStorage.setItem("Score", monNouveauScore.toString()+"%");
+    if(nbSlide === 0){
+        sessionStorage.setItem("Score", "100%");
+    }else {
+        monScore = sessionStorage.getItem("Score");
+        monScore = monScore.substring(0, monScore.length - 1);
+        monNouveauScore = parseInt(parseInt(monScore) + ((100 - parseInt(monScore)) / nbSlide));
+        sessionStorage.setItem("Score", monNouveauScore.toString() + "%");
+    }
 }
