4f65f139253731d594dfb98173906d319bf6cea1
Léa <leagr@LAPTOP-AVJKEBSL.localdomain>
2023-02-16 11:17:22 +0100

    page facultatif est ok

--- soleil/public/js/slide7.js
+++ soleil/public/js/slide7.js
@@ -6,7 +6,7 @@ const initSlide7 = async function(){
         executed7 = true;
         $("#facultatif footer button").eq(0).click(function(){
             let checklist = ['facul1', 'facul2', 'facul3', 'facul4'];
-            checklist = checklist.map(elt => $(`#${elt}`).is(':checked'));
+            checklist = checklist.map(elt => $(`#${elt}`).attr("alt"));
             console.log(checklist);
             quiz["facultatif"] = checklist;
             
@@ -15,6 +15,20 @@ const initSlide7 = async function(){
         $("#facultatif footer .passer").click(function(){
             swiper.slideNext();
             quiz["facultatif"] = [];
+        })
+
+        $("#facultatif #bulle-question img").click(function(){
+            let alt = $(this).attr("alt");
+            
+            if(alt == "1"){
+                $(this).attr("src",  $(this).attr("src").replace("selection", "non selection"))
+                $(this).attr("alt", "0");
+            }
+            else{
+                $(this).attr("src",  $(this).attr("src").replace("non selection", "selection"))
+                $(this).attr("alt", "1");
+            }
+            
         })
     }
 }
